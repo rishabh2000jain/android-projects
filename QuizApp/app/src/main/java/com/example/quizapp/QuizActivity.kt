eeaaab0d8fb1aa356afcb2310c809fff2cc83efe
Rishabh Jain <rishabh.jain@unthinkable.co>
2023-02-15 11:48:16 +0530

    completed quiz screen

--- QuizApp/app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -2,11 +2,166 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.quizapp.databinding.ActivityQuizBinding
+
 
 class QuizActivity : AppCompatActivity() {
+    private var selectedOption:Int?=null
+    private lateinit var viewModel: QuizViewModel
+    private lateinit var binding: ActivityQuizBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
+        binding = ActivityQuizBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[QuizViewModel::class.java]
+        getQuestions()
+        getProgress()
+        binding.progressbar.visibility = View.INVISIBLE
+        binding.questionView.visibility = View.VISIBLE
+        onSubmit()
+        handleOptionSelect()
+        buttonStateChange()
+        handleCorrectAns()
     }
+    private fun getQuestions(){
+        viewModel.getCurrentQuestion().observe(this) {
+            setQuestion(it)
+        }
+    }
+    private fun getProgress(){
+        viewModel.getQuestionsProgress().observe(this){
+            binding.remainingQuestionTxt.text = "$it/${viewModel.getTotalQuestions()}"
+            binding.questionsProgressBar.progress = ((it.toDouble()/viewModel.getTotalQuestions())*100).toInt()
+        }
+    }
+
+   private fun setQuestion(question: Question){
+        resetOptions()
+        binding.questionTxt.text = question.question
+        binding.flagImg.background = resources.getDrawable(question.imagePath,theme)
+        binding.option1.text = question.option1
+        binding.option2.text = question.option2
+        binding.option3.text = question.option3
+        binding.option4.text = question.option4
+    }
+
+    private fun onSubmit(){
+        binding.submitBtn.setOnClickListener {
+            when(viewModel.getButtonState().value){
+                ButtonState.Submit -> {
+                    selectedOption?.let {
+                        viewModel.submitAnswer(optionNumber = it)
+                    }
+                }
+                ButtonState.Finish -> {
+                        // TODO:navigate to final screen
+                }
+                ButtonState.NextQuestion -> {
+                    viewModel.getNextQuestion()
+                }
+                null -> {
+
+                }
+            }
+        }
+    }
+
+    private fun handleOptionSelect(){
+        binding.option1.setOnClickListener {
+            selectedOption=1
+            resetOptions()
+            binding.option1.setStrokeColorResource(R.color.purple_500)
+        }
+        binding.option2.setOnClickListener {
+            selectedOption=2
+            resetOptions()
+            binding.option2.setStrokeColorResource(R.color.purple_500)
+        }
+        binding.option3.setOnClickListener {
+            selectedOption=3
+            resetOptions()
+            binding.option3.setStrokeColorResource(R.color.purple_500)
+        }
+        binding.option4.setOnClickListener {
+            selectedOption=4
+            resetOptions()
+            binding.option4.setStrokeColorResource(R.color.purple_500)
+        }
+    }
+
+    private fun resetOptions(){
+        binding.option1.setStrokeColorResource(android.R.color.darker_gray)
+        binding.option2.setStrokeColorResource(android.R.color.darker_gray)
+        binding.option3.setStrokeColorResource(android.R.color.darker_gray)
+        binding.option4.setStrokeColorResource(android.R.color.darker_gray)
+        binding.option1.setBackgroundColor(resources.getColor(R.color.white,theme))
+        binding.option2.setBackgroundColor(resources.getColor(R.color.white,theme))
+        binding.option3.setBackgroundColor(resources.getColor(R.color.white,theme))
+        binding.option4.setBackgroundColor(resources.getColor(R.color.white,theme))
+        changeButtonClickOnOptions(enabled = true)
+    }
+
+    private fun buttonStateChange(){
+        viewModel.getButtonState().observe(this){ it ->
+            binding.submitBtn.text = it.btnText
+        }
+    }
+
+    private fun handleCorrectAns(){
+        viewModel.getCorrectAns().observe(this){
+            it?.let{
+                setCorrectOptionColor(it)
+                if(it != selectedOption){
+                    setWrongOptionColor(selectedOption!!)
+                }
+
+            }
+        }
+    }
+    private fun setCorrectOptionColor(option:Int){
+        changeButtonClickOnOptions(enabled=false)
+        when(option){
+            1 -> {
+                binding.option1.setBackgroundColor(resources.getColor(android.R.color.holo_green_light,theme))
+            }
+            2 -> {
+                binding.option2.setBackgroundColor(resources.getColor(android.R.color.holo_green_light,theme))
+            }
+            3 -> {
+                binding.option3.setBackgroundColor(resources.getColor(android.R.color.holo_green_light,theme))
+            }
+            4 -> {
+                binding.option4.setBackgroundColor(resources.getColor(android.R.color.holo_green_light,theme))
+
+            }
+        }
+    }
+    private fun setWrongOptionColor(option:Int){
+        changeButtonClickOnOptions(enabled=false)
+        when(option){
+            1 -> {
+                binding.option1.setBackgroundColor(resources.getColor(android.R.color.holo_red_light,theme))
+            }
+            2 -> {
+                binding.option2.setBackgroundColor(resources.getColor(android.R.color.holo_red_light,theme))
+            }
+            3 -> {
+                binding.option3.setBackgroundColor(resources.getColor(android.R.color.holo_red_light,theme))
+            }
+            4 -> {
+                binding.option4.setBackgroundColor(resources.getColor(android.R.color.holo_red_light,theme))
+
+            }
+        }
+    }
+    private fun changeButtonClickOnOptions(enabled:Boolean){
+        binding.option1.isEnabled=enabled
+        binding.option2.isEnabled=enabled
+        binding.option3.isEnabled=enabled
+        binding.option4.isEnabled=enabled
+    }
+
 }
--- QuizApp/app/src/main/java/com/example/quizapp/QuizViewModel.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizViewModel.kt
@@ -1,15 +1,74 @@
 package com.example.quizapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 
 class QuizViewModel: ViewModel() {
-    var userName:String? = null
-    companion object{
-        val Factory : ViewModelProvider.Factory = object :ViewModelProvider.Factory{
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return QuizViewModel() as T
+    private var totalQuestion:Int = 0
+    private val correctAnswerCount:MutableLiveData<Int> = MutableLiveData(0)
+    private val correctAnswer:MutableLiveData<Int?> = MutableLiveData(null)
+    private val currentQuestion:MutableLiveData<Question> = MutableLiveData()
+    private var buttonState:MutableLiveData<ButtonState> =MutableLiveData(ButtonState.Submit)
+    init {
+        currentQuestion.value = Constants.getQuestions()[0]
+        totalQuestion = Constants.getQuestions().size
+    }
+
+    fun getCurrentQuestion():LiveData<Question>{
+        return currentQuestion
+    }
+    fun getQuestionsProgress():LiveData<Int>{
+        return correctAnswerCount
+    }
+    fun  getTotalQuestions():Int{
+        return totalQuestion
+    }
+    fun  getCorrectAns():LiveData<Int?>{
+        return correctAnswer
+    }
+
+    fun submitAnswer(optionNumber:Int){
+        currentQuestion.value?.let {
+            if(it.ansOption == optionNumber){
+                correctAnswerCount.value  = correctAnswerCount.value?.plus(1)
+            }
+            correctAnswer.value = it.ansOption
+            currentQuestion.value?.let {
+                if(it.questionNumber == Constants.getQuestions().size){
+                    updateButtonState(ButtonState.Finish)
+                    return
+                }else{
+                    updateButtonState(ButtonState.NextQuestion)
+                }
             }
         }
+
     }
+
+    fun getButtonState():LiveData<ButtonState> = buttonState
+
+     fun getNextQuestion(){
+         correctAnswer.value=null
+        currentQuestion.value?.let {
+            if(it.questionNumber == Constants.getQuestions().size){
+                updateButtonState(ButtonState.Finish)
+                return
+            }
+            currentQuestion.value = Constants.getQuestions()[it.questionNumber]
+            updateButtonState(ButtonState.Submit)
+        }
+    }
+
+
+    private fun updateButtonState(type:ButtonState){
+        buttonState.value = type
+    }
+
+}
+
+enum class ButtonState(val btnText:String){
+    Submit("Submit"),
+    Finish("Finish"),
+    NextQuestion("Next Question")
 }
